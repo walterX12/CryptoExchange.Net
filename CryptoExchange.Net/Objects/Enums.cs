ff6c02e574d3f8a270e2960fdda0222c53457868
Jkorf <jankorf91@gmail.com>
2021-07-05 10:34:25 +0200

    Added some helper methods, some code comments

--- CryptoExchange.Net/Objects/Enums.cs
+++ CryptoExchange.Net/Objects/Enums.cs
@@ -101,4 +101,19 @@
         /// </summary>
         Array
     }
+
+    /// <summary>
+    /// How to round
+    /// </summary>
+    public enum RoundingType
+    {
+        /// <summary>
+        /// Round down (flooring)
+        /// </summary>
+        Down,
+        /// <summary>
+        /// Round to closest value
+        /// </summary>
+        Closest
+    }
 }
